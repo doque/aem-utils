9bc23a9efcf1facef3b24f57d68581153eeffbfd
Dominique Busser <dominique.busser@comvel.de>
2015-05-12 10:47:39 +0200

    branch fix

---
+++ wcmmode/index.js
@@ -0,0 +1,11 @@
+require(["http://cdn.rawgit.com/ryanburnette/Qurl/master/qurl.js"], function() {
+  var url = Qurl.create();
+  if ("wcmmode" in url.query()) {
+    // wcmmode exists, remove it
+    url.query("wcmmode", false);
+  } else {
+    // default new wcmmode is disabled
+    url.query("wcmmode", "disabled");
+    location.reload();
+  }
+});
